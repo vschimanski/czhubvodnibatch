2656561e7b9e2f640bca07689283ca2e20916cdf
Your Name <you@example.com>
2025-05-04 18:26:46 +0200

    init

---
+++ src/test/java/cz/hub/vodni/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.hub.vodni.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
